e607e0e9aebf967f27de6095a726674c7851cdfd
Siddeshwar <siddeshwar.vasam@gmailcom>
2024-01-29 14:51:32 +0530

    "Ntgh"

--- Book.java
+++ Book.java
@@ -48,7 +48,7 @@ public class Book{
         this.price = price;
     }
     public static void main(String[] args) {
-        Book book1 = new Book("To Kill a Mockingbird", "Harper Lee", 4.27, 15.99);
+        Book book1 = new Book("To Kill a Mockngbird", "Harper Lee", 4.27, 15.99);
         Book book2 = new Book("1984", "George Orwell", 4.17, 12.99);
         Book book3 = new Book("Moby-Dick", "Herman Melville", 3.5, 14.99);
         Book book4 = new Book("Pride and Prejudice", "Jane Austen", 4.25, 10.99);
