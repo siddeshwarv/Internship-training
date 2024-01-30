e56d5fa5566a29b11801a0a9999d177b681e0bfd
Siddeshwar <siddeshwar.vasam@gmailcom>
2024-01-30 09:48:32 +0530

    Assignment-1 Completed

--- Assignment1.java
+++ Assignment1.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.Period;
 import java.time.ZoneId;
-import java.time.Duration;
+import java.time.ZonedDateTime;
 // import java.time.;
 public class Assignment1 {
     
@@ -16,14 +13,26 @@ public class Assignment1 {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime userDateTime=null;
         try {
+            // Parsing datetime of user input
             userDateTime  = LocalDateTime.parse(userInput,formatter);
             System.out.println("date time:"+userDateTime);
+            // using ZoneId function to specify the user date time zone
+            ZoneId itsZone = ZoneId.of("Asia/Kolkata");
+            ZonedDateTime userZoneDateTime = userDateTime.atZone(itsZone);
+            // Converting user datetime to unix time stamp
+            long unixTimeStamp = userZoneDateTime.toEpochSecond();
+            System.out.println("Unix Time Stamp:"+unixTimeStamp);
+            // Converting user ist time stamp to utc time stamp 
+            ZonedDateTime utcDateTime = userZoneDateTime.withZoneSameInstant(ZoneId.of("UTC"));
+            System.out.println(utcDateTime);
+            // Converting utc to IST time stamp
+            ZonedDateTime istDateTime = utcDateTime.withZoneSameInstant(itsZone);
+            System.out.println("IST date time :"+istDateTime);
         }
         catch(Exception e){
             System.out.println("Reenter date and time in right format."+e);
         }
-        ZoneId itsZone = ZoneId.of("Asia/Kolkata");
-        System.out.println(userDateTime.atZone(itsZone).toEpochSecond());
+        
 
     }
 }
